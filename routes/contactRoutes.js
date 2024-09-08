9a8c6e4b46b91fa213f94b64ce9f41bb786103d2
Harrisonk76 <165740611+Harrisonk76@users.noreply.github.com>
2024-09-08 10:50:16 -0700

    fixed post

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const contactController = require("./contactController");
+const contactController = require("../controllers/contactController");
 
 const {
   getContacts,
@@ -9,7 +9,7 @@ const {
   getContact,
   updateContact,
   deleteContact,
-} = require("./contactController");
+} = require("../controllers/contactController");
 
 router.route("/").get(getContacts).post(createContact);
 router.route("/:id").get(getContact).put(updateContact).delete(deleteContact);
